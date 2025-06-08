7b0f78a405b3f42c7974c2cca01ca984df928b24
deepsha-3 <itsmedeepsha375@gmail.com>
2025-06-08 18:17:10 +0545

    Files changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: "./",
+ base: "/Portfolio_files",
  build: {
   outDir: 'dist',
   emptyOutDir: true
