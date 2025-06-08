664e9b3df3598e14e3962be4ee7d760b079734a4
deepsha-3 <itsmedeepsha375@gmail.com>
2025-06-08 18:27:11 +0545

    File changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: "/Portfolio_files/",
+ base: "/",
+ plugins: [react()],
  build: {
   outDir: 'dist',
   emptyOutDir: true
